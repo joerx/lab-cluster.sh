fc45c4c3a7d606e14d0e661986c42ffe4ff259f0
joerx <1712460+joerx@users.noreply.github.com>
2025-08-09 12:52:58 +0800

    Add cert-manager

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -106,6 +106,8 @@ spec:
   sourceRepos:
   - '$REPO_URL'
   - 'https://kubernetes.github.io/ingress-nginx'
+  - 'https://prometheus-community.github.io/helm-charts'
+  - 'https://charts.jetstack.io'
   destinations:
   - namespace: '*'
     server: '*'
