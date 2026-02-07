8c97a493206a2710a88f5bd4f351932a05241341
joerx <1712460+joerx@users.noreply.github.com>
2026-02-07 16:40:49 +0000

    Refactor k3s provider

--- scripts/providers/k3s/_install-k3s.sh
+++ scripts/providers/k3s/_install-k3s.sh
@@ -2,31 +2,47 @@
 
 set -e -o pipefail
 
-NAME=$1
+usage() {
+  >&2 echo "$0 <name> <ip-address>"
+}
 
-VMDIR=./vms/$NAME
+log() {
+  >&2 echo "$@"
+}
 
-KUBECFG_OUT="$VMDIR/.kubecfg"
+NAME="$1"
+IP_ADDR="$2"
 
-if [[ -z $NAME ]]; then
-  >&2 echo "$0 <NAME>"
+if [[ -z "$NAME" || -z "$IP_ADDR" ]]; then
+  usage
   exit 1
 fi
 
-IP_ADDR=$(./vm-addr.sh $NAME 20)
+BASEDIR=$(dirname $0)
+VMDIR=$(realpath $BASEDIR/.vms)/$NAME
+
+KUBECFG_OUT="$VMDIR/.kubecfg"
 
 SSH_OPTS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
-echo "VM '$NAME' has IP address: $IP_ADDR"
+# SSH login may fail on first attempt, add a retry loop to work around that
+for i in {1..5}; do
+  if ssh $SSH_OPTS -t centos@$IP_ADDR 'echo "connected to $HOSTNAME"'; then
+    log "SSH connection successful on attempt $i"
+    break
+  fi
+  log "SSH connection failed, retrying in 3 seconds..."
+  sleep 3
+done
 
 # Install k3s inside the VM. We disable traefik and will install ingress-nginx instead later.
 ssh $SSH_OPTS -t centos@$IP_ADDR 'echo "connected to $HOSTNAME"; curl -fL https://get.k3s.io | sh -s - --disable=traefik --write-kubeconfig-mode=644'
 
 # Fetch the kubeconfig file from the VM, replacing the server with the VM's IP address
 ssh $SSH_OPTS centos@$IP_ADDR -- cat /etc/rancher/k3s/k3s.yaml | sed "s/server: .*$/server: https:\/\/$IP_ADDR:6443/" > $KUBECFG_OUT
-echo "Kubernetes config written to $KUBECFG_OUT"
+log "Kubernetes config written to $KUBECFG_OUT"
 
 # Validate it worked and we can successfully connect to the cluster
 CMD="KUBECONFIG=$KUBECFG_OUT kubectl cluster-info"
-echo "Running '$CMD' to validate"
+log "Running '$CMD' to validate"
 eval $CMD
--- scripts/providers/k3s/create.sh
+++ scripts/providers/k3s/create.sh
@@ -2,41 +2,38 @@
 
 set -e -o pipefail
 
-OS_VARIANT=centos-stream9
-OS_ARCH=$(uname -m)
-
-# TODO: make this dynamic, download image if not exists
-IMGDIR=$(realpath $(dirname $0)/images)
-
-BASE_IMG=${OS_VARIANT}.${OS_ARCH}.qcow2
-
-NAME=$1
-
-VMDIR=vms/$NAME
-
-
 log() {
   >&2 echo "$@"
 }
 
 usage() {
   >&2 echo "$0 <name>"
-  exit 1
 }
 
 
-if [[ -z "$NAME" ]]; then
-  usage
-fi
+OS_VARIANT=centos-stream9
+OS_ARCH=$(uname -m)
 
-if [[ ! -f "$IMGDIR/$BASE_IMG" ]]; then
-  >&2 echo "Base image for '$BASE_IMG' not found, please download and place it in $IMGDIR"
-  exit 1
-fi
+IMG_URL="https://cloud.centos.org/centos/9-stream/${OS_ARCH}/images/CentOS-Stream-GenericCloud-9-latest.${OS_ARCH}.qcow2"
+
+NAME=${1:-"k3s-lab-$(hostname)"}
+
+log "Creating VM '$NAME' with OS variant '$OS_VARIANT' and architecture '$OS_ARCH'"
+
+BASEDIR=$(dirname $0)
+IMGDIR=$(realpath $BASEDIR/../../../images)
+VMDIR=$(realpath $BASEDIR/.vms)/$NAME
+
+BASE_IMG=${OS_VARIANT}.${OS_ARCH}.qcow2
 
 if [[ -d $VMDIR ]]; then
   >&2 echo "VM already exists, to re-create, run vm-destroy.sh first"
   exit 1
+fi
+
+if [[ ! -f "$IMGDIR/$BASE_IMG" ]]; then
+  log "Base image for '$BASE_IMG' not found, downloading it to $IMGDIR/$BASE_IMG - this may take a while..."
+  curl -fL $IMG_URL -o $IMGDIR/$BASE_IMG
 fi
 
 mkdir -p $VMDIR
@@ -70,8 +67,8 @@ genisoimage -output $VMDIR/cidata.iso -V cidata -r -J $VMDIR/user-data $VMDIR/me
 # Virt-install
 virt-install \
     --name $NAME \
-    --ram 2048 \
-    --vcpus 2 \
+    --ram 4096 \
+    --vcpus 4 \
     --import \
     --disk path=$VMDIR/$NAME.qcow2,format=qcow2 \
     --disk path=$VMDIR/cidata.iso,device=cdrom \
@@ -83,11 +80,24 @@ virt-install \
 log "VM '$NAME' created."
 log "Getting IP address (this may take a few seconds)"
 
-IP_ADDR=$(./vm-addr.sh $NAME 20)
+IP_ADDR=$($BASEDIR/_get-addr.sh $NAME 20)
 
+log "Installing k3s on the VM '$NAME' at IP '$IP_ADDR' (this may take a few minutes)..."
+
+$BASEDIR/_install-k3s.sh $NAME $IP_ADDR
+
+log ""
+log "--------------------------------------------------------------------------"
+log "VM '$NAME' is ready and running k3s at IP address '$IP_ADDR'"
 log "IP for '$NAME' is '$IP_ADDR'"
 log "To add it to /etc/hosts:"
 log "echo $IP_ADDR $NAME.local $NAME | sudo tee -a /etc/hosts"
 log ""
-log "To connect:"
-log "ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" centos@192.168.122.28"
+log "To connect via SSH:"
+log "ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" centos@$IP_ADDR"
+log ""
+log "To connect with kubectl:"
+log "export KUBECONFIG=$VMDIR/.kubecfg"
+log "kubectl cluster-info"
+log "--------------------------------------------------------------------------"
+log ""
