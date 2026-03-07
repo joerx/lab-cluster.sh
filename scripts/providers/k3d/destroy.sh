ce8076d555368e2ad781f5684d9664f8436ba66b
Jörg Henning <1712460+joerx@users.noreply.github.com>
2026-03-07 19:11:35 +0000

    Refactor bootstrap to be helm based  (#8)
    * Initial TF stuff
    * Refactor bootstrap to use helm only
    * Refactor k3d delete script
    * Publish helm charts to GHCR

--- scripts/providers/k3d/destroy.sh
+++ scripts/providers/k3d/destroy.sh
@@ -2,4 +2,13 @@
 
 set -euo pipefail
 
-k3d cluster delete k3s-default
+log() {
+  >&2 echo "$@"
+}
+
+NAME=${1:-"k3d-lab-$(hostname)"}
+
+log "This will delete cluster '$NAME' and all data on it. Continue? (Ctrl-C to abort)"
+read
+
+k3d cluster delete $NAME
